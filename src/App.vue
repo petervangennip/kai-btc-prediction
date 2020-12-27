43882022d0619e63410645caeda9bc056b36d49a
Peter van Gennip <peter.van.gennip@valtech.com>
2020-12-27 20:14:55 +0100

    chore: add visuals for current leader

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,17 @@
               <th>Name</th>
               <th>Price prediction</th>
             </tr>
-            <tr v-for="(member, i) in closestPrediction" :key="i">
-              <!-- <td :class="{ active: member.name === closest.name }"> -->
+            <tr
+              v-for="(member, i) in sortedValues"
+              :key="i"
+              :class="{ active: member.name === getBestPredictor.name }"
+            >
               <td>
                 {{ member.name }}
               </td>
-              <td>{{ member.prediction }}</td>
+              <td>
+                {{ member.prediction }}
+              </td>
             </tr>
           </table>
         </div>
@@ -49,19 +54,40 @@ export default {
     cryptoData() {
       return this.$store.state.appData;
     },
-    closestPrediction() {
+    sortedValues() {
       // eslint-disable-next-line vue/no-side-effects-in-computed-properties
       return this.cryptoData.sort((a, b) => a.prediction - b.prediction);
-      // .find(g => g.prediction >= this.info.rate_float);
+    },
+    getBestPredictor() {
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      return this.cryptoData
+        .sort((a, b) => a.prediction - b.prediction)
+        .find(g => g.prediction === this.closestPrediction);
+    },
+    closestPrediction() {
+      return this.getClosestPrediction(this.cryptoData, this.info.rate_float);
     }
   },
+  methods: {
+    getData() {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => {
+          this.isLoaded = true;
+          this.info = response.data.bpi.USD;
+        });
+    },
+    getClosestPrediction(cryptos, target) {
+      return cryptos.reduce((acc, crypto) => {
+        return Math.abs(crypto.prediction - target) < Math.abs(acc - target)
+          ? crypto.prediction
+          : acc;
+      }, 0);
+    }
+  },
+
   mounted() {
-    axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => {
-        this.isLoaded = true;
-        this.info = response.data.bpi.USD;
-      });
+    this.getData();
   }
 };
 </script>
@@ -82,6 +108,20 @@ export default {
   }
 
   table {
+    border-spacing: 0;
+    border-collapse: collapse;
+
+    tr {
+      &.active {
+        border: solid 1px #000;
+        background-color: #f8931a;
+
+        td {
+          color: #000;
+        }
+      }
+    }
+
     td {
       width: 50%;
     }
